909cad697ab78eda4fdf261351a3589795e05627
Towphe <castillochristopherjames@gmail.com>
2021-11-23 21:02:10 +0800

    Configured Razor Pages; made Home and Error pages

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Npgsql;
 using System;
 using System.Collections.Generic;
