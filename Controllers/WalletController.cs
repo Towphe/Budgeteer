7f5a829302f5e0b415028acaef58a15101740c90
Towphe <castillochristopherjames@gmail.com>
2021-11-24 16:56:40 +0800

    Made /Wallet controller and Index view.

---
+++ Controllers/WalletController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Budgeteer.Models;
+
+namespace Budgeteer.Controllers
+{
+    public class WalletController : Controller
+    {
+        public WalletController(budgeteer_appdbContext appDBCtx)
+        {
+            AppDBCtx = appDBCtx;
+        }
+        private budgeteer_appdbContext AppDBCtx { get; set; }
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Wallet";
+            return View();
+        }
+    }
+}
